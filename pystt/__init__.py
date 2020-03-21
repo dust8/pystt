af7393a2d16858f43b776d94511fd36c9c8e5312
nws0507 <1090067014@qq.com>
2020-03-21 19:38:10 +0800

    二改了pystt库，支持多层反序列化

--- pystt/__init__.py
+++ pystt/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '0.2.2'
 
-from .stt import dumps, loads
+from .stt import PyStt
--- pystt/utils.py
+++ pystt/utils.py
@@ -8,3 +8,8 @@ def unescape_string(s):
     s = s.replace("@A", "@")
     s = s.replace('@S', '/')
     return s
+
+
+def first_unescape_string(s):
+    s = s.replace("@A", "@")
+    return s
